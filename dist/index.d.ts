5e143191e87d374542d9a23163d39029828a0ff3
thamaraiselvam <thamaraiselvam.thangavel@iff.com>
2019-02-12 12:46:39 +0530

    dist dir removed from git

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './ionic-chips.module';
-export * from './components/chips/chips';
-export * from './components/modal/modal';
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export * from './ionic-chips.module';
-export * from './components/chips/chips';
-export * from './components/modal/modal';
-//# sourceMappingURL=index.js.map
